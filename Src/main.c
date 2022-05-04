fb83730d75f7c4142c199e6d1955a45d0b9a9be8
bhaktialpha <bhakti.deshpande@alpha-ict.in>
2022-05-04 17:31:27 +0530

    v3.8 - desktop app issue fixed..debug off

--- Src/main.c
+++ Src/main.c
@@ -39,7 +39,7 @@ void RTC_TimeShow(uint8_t* showtime);
 //unsigned int test_num;
 extern unsigned char LPUART1_Image_Data_Buffer[16803];
 extern unsigned int Second_count;
-uint8_t FW_Version[20]="ID_TAG_Phase2_V3.7\r\n";
+uint8_t FW_Version[20]="ID_TAG_Phase2_V3.8\r\n";
 uint8_t BLE_Counter[33];
 uint8_t ADC_Counter[16];
 GPIO_PinState Key_status;
